c7f0b18ba13cf3cc009939a0677a752bd04c18f5
BeRealQueally <129683130+BeRealQueally@users.noreply.github.com>
2024-01-16 18:35:18 +0800

    fix: make the output JSON

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ def get_url_data(host: str, cert_str: str, file_name: str | None):
                                      headers=headers)
         r = urllib.request.urlopen(req, context=context)
     except:
-        print({'status': 'error'})
+        print(json.dumps({"status": "error"}))
     else:
         if r.status // 200 != 1:
-            print({'status': 'error'})
+            print(json.dumps({"status": "error"}))
         if output_str := json.loads(r.read().decode('utf-8')).get('serverCertificate'):
-            print({'status': 'success', 'cert': output_str})
+            print(json.dumps({"status": "success", "cert": output_str}))
             if file_name:
                 open(file_name, 'wb').write(base64.b64decode(output_str))
         else:
