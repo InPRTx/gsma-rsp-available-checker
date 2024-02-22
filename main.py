a2ecfe36860763ec2e5d9773efc5a4b98d79465f
BeRealQueally <129683130+BeRealQueally@users.noreply.github.com>
2024-02-23 01:07:38 +0800

    refactor: multi ci support and better euiccinfo1 generator

--- main.py
+++ main.py
@@ -13,46 +13,79 @@ import urllib.request
 context = ssl._create_unverified_context()
 
 
-def get_euicc_info(cert_str: str) -> str:
-    a = b'\xBF\x20\x35\x82\x03\x02\x02\x00\xA9\x16\x04\x14' + bytes.fromhex(
-        cert_str) + b'\xaa\x16\x04\x14' + bytes.fromhex(cert_str)
-    return base64.encodebytes(a).decode().strip()
+def gen_tlv(tag: bytes, data: bytes) -> bytes:
+    length = len(data)
+    tlv = tag + bytes([length]) + data
+    return tlv
+
+
+def gen_svn(svn_str: str) -> bytes:
+    tag = b'\x82'
+    svn = [int(i) for i in svn_str.split('.')]
+    return gen_tlv(tag, bytes(svn))
+
+
+def gen_pki(cert: bytes) -> bytes:
+    tag = b'\x04'
+    return gen_tlv(tag, cert)
+
+
+def gen_pki_list(tag: bytes, cert_str: str) -> bytes:
+    certs = cert_str.split(',')
+    pki_list = b''
+    for cert in certs:
+        if re.match(r'^[0-9a-fA-F]{40}$', cert):
+            cert = bytes.fromhex(cert)
+            pki_list += gen_pki(cert)
+        else:
+            raise ValueError('证书应为40位0-f或0-F,多条证书以逗号隔开')
+    return gen_tlv(tag, pki_list)
+
+
+def gen_euicc_info(cert_str: str) -> str:
+    tag = b'\xBF\x20'
+    euiccinfo = gen_tlv(tag,
+                        gen_svn('2.2.2')
+                        + gen_pki_list(b'\xA9', cert_str)
+                        + gen_pki_list(b'\xAA', cert_str))
+    return base64.encodebytes(euiccinfo).decode().strip()
 
 
 def gen_random_challenge() -> str:
     return base64.b64encode(os.urandom(16)).decode().strip()
 
 
-def get_url_data(host: str, cert_str: str, file_name: str | None):
-    data = {"smdpAddress": host, "euiccChallenge": gen_random_challenge(),
-            "euiccInfo1": get_euicc_info(cert_str)}
-    headers = {'Content-Type': 'application/json',
-               'User-Agent': 'gsma-rsp-lpad',
-               'X-Admin-Protocol': 'gsma/rsp/v2.2.0'}
+def request_cert(host: str, cert_str: str, file_name: str | None):
+    body = {"smdpAddress": host, "euiccChallenge": gen_random_challenge(),
+            "euiccInfo1": gen_euicc_info(cert_str)}
+    header = {'Content-Type': 'application/json',
+              'User-Agent': 'gsma-rsp-lpad',
+              'X-Admin-Protocol': 'gsma/rsp/v2.2.0'}
 
     try:
         req = urllib.request.Request(f'https://{host}/gsma/rsp2/es9plus/initiateAuthentication',
-                                     data=json.dumps(data).encode('utf-8'),
-                                     headers=headers)
-        r = urllib.request.urlopen(req, context=context)
+                                     data=json.dumps(body).encode('utf-8'),
+                                     headers=header)
+        resp = urllib.request.urlopen(req, context=context)
     except:
         print(json.dumps({"status": "error"}))
     else:
-        if r.status // 200 != 1:
-            print(json.dumps({"status": "error", "statuscode": r.status}))
-        if resp := r.read().decode('utf-8'):
-            if cert := json.loads(resp.replace(r'\n', '')).get('serverCertificate'):
+        if resp.status // 200 != 1:
+            print(json.dumps({"status": "error", "statuscode": resp.status}))
+        if data := resp.read().decode('utf-8'):
+            if cert := json.loads(data.replace(r'\n', '')).get('serverCertificate'):
                 certificate = x509.load_der_x509_certificate(base64.b64decode(cert), default_backend())
                 keyid = certificate.extensions.get_extension_for_oid(
                     x509.ExtensionOID.AUTHORITY_KEY_IDENTIFIER).value.key_identifier.hex()
-                if keyid == cert_str:
+                if keyid in cert_str:
                     print(json.dumps({"status": "success", "keyID": keyid, "cert": cert}))
                     if file_name:
                         open(file_name, 'wb').write(base64.b64decode(cert))
                 else:
-                    print(json.dumps({"status": "fail", "reason": "KeyIDMismatch", "keyID": keyid,  "cert": cert}))
+                    print(json.dumps({"status": "fail", "reason": "KeyIDMismatch", "keyID": keyid, "cert": cert}))
             else:
-                print(json.dumps({"status": "fail", "reason": "CertNotFound", "resp": json.loads(resp).get('header')}))
+                print(json.dumps(
+                    {"status": "fail", "reason": "CertNotFound", "response": json.loads(data).get('header')}))
         else:
             print(json.dumps({"status": "fail", "reason": "none"}))
 
@@ -63,6 +96,4 @@ if __name__ == '__main__':
     parser.add_argument('-c', required=True, help='证书KI')
     parser.add_argument('-w', help='保存文件名')
     args = parser.parse_args()
-    if not re.match(r'^[0-9a-fA-F]{40}$', args.c):
-        raise ValueError('证书应为40位0-f或0-F')
-    get_url_data(args.s, args.c.lower(), args.w)
+    request_cert(args.s, args.c.lower(), args.w)
